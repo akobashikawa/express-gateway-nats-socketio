bb6bb13de50de47e39a9f2300549f5fef9ff5075
Antonio Kobashikawa <akobashikawa@gmail.com>
2024-11-04 06:15:04 -0500

    Fix restToService

--- controller.js
+++ controller.js
@@ -3,30 +3,30 @@ class Controller {
     constructor({ service }) {
         this.service = service;
     }
-    
+
     async hello(req, res) {
         const name = req.query.name;
         const message = await this.service.hello(name);
         res.send(message);
     }
 
-    async productos(req, res) {
+    async restToService(req, res) {
         const { method, params, query, body } = req;
-        const productos = await this.service.requestReply({ subject: 'productos.requestReply', method, params, query, body });
+        console.log(params)
+        // Dividir la cadena en partes
+        const parts = params[0].split('/');
+        const serviceName = parts[0]; // El primer elemento es el nombre del servicio
+
+        // Crear un objeto para newParams
+        const newParams = {};
+        parts.slice(1).forEach((param, index) => {
+            if (param) {
+                newParams[index] = param;
+            }
+        });
+        const productos = await this.service.requestReply({ subject: `${serviceName}.requestReply`, method, params: newParams, query, body });
         // const productos = await this.service.manualRequestReply({ subject: 'productos.request', responseSubject: 'productos.response', method, params, query, body });
         res.json(productos);
-    }
-
-    async personas(req, res) {
-        const { method, params, query, body } = req;
-        const personas = await this.service.requestReply({ subject: 'personas.requestReply', method, params, query, body });
-        res.json(personas);
-    }
-
-    async ventas(req, res) {
-        const { method, params, query, body } = req;
-        const ventas = await this.service.requestReply({ subject: 'ventas.requestReply', method, params, query, body });
-        res.json(ventas);
     }
 
 }
--- router.js
+++ router.js
@@ -11,8 +11,6 @@ const controller = new Controller({ service });
 
 router.all('/hello', async (req, res) => controller.hello(req, res) );
 
-router.all('/productos', async (req, res) => controller.productos(req, res) );
-router.all('/personas', async (req, res) => controller.personas(req, res) );
-router.all('/ventas', async (req, res) => controller.ventas(req, res) );
+router.all('/*', async (req, res) => controller.restToService(req, res) );
 
 module.exports = router;
